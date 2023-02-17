a36065c88838093d2a84a908487b9b8572236dc1
wsehyeon <wsehyeon21@gmail.com>
2023-02-17 17:17:19 +0900

    add : Define.hpp file

---
+++ includes/Define.hpp
@@ -0,0 +1,18 @@
+#ifndef __DEFINE_HPP__
+#define __DEFINE_HPP__
+
+// just define..... for test
+# define N "\033[0m"
+# define R "\033[31m"
+# define G "\033[32m" 
+# define Y "\033[33m" 
+# define B "\033[34m" 
+
+# define PRINT_LOG(fd, str, color) std::cout << color <<  str << "[" << fd << "]" << N << std::endl;
+
+# define PRINT_MSG(fd, str, buffer, color) std::cout << color <<  str << "[" << fd << "]: " << buffer << N << std::endl;
+
+# define PRINT_EVENT(ident, flags, filter, fflags, data, udata, color) std::cout << color << "[" << ident << "]: "\
+                        << flags << " " << filter << " " << fflags << " " << data << " " << udata << " " <<  N << std::endl;
+
+#endif
