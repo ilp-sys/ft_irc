809a45af644d2cad6808f9c2c695b128a46ef165
wsehyeon <wsehyeon21@gmail.com>
2023-02-22 14:16:16 +0900

    Add : Part command JOIN 과 연계한 테스트 필요

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ RM				:= rm
 RMFLAGS     	:= -rf
 
 SRCDIR			:= ./srcs/
-SRCNAME 		:= Client.cpp Command.cpp CommandInvoker.cpp Join.cpp Kick.cpp Nick.cpp Notice.cpp Part.cpp Privmsg.cpp Quit.cpp Server.cpp User.cpp main.cpp Channel.cpp Join.cpp
+SRCNAME 		:= Client.cpp Command.cpp CommandInvoker.cpp Join.cpp Kick.cpp Nick.cpp Notice.cpp Part.cpp Privmsg.cpp Quit.cpp Server.cpp User.cpp main.cpp Channel.cpp Join.cpp Part.cpp
 
 SRCS			:= $(addprefix $(SRCDIR), $(SRCNAME))
 
--- srcs/Part.cpp
+++ srcs/Part.cpp
@@ -1,6 +1,40 @@
 #include "../includes/Part.hpp"
+#include "../includes/Define.hpp"
+#include "../includes/Server.hpp"
+#include <sstream>
 
-void Part::execute(Client &client)
-{
+Part::Part() : Command(0){};
+
+void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
+		std::vector<struct kevent>& changelist, std::map<std::string, Channel>* channels){
+	Server& server = Server::getInstance();
+	Channel *targetChannel;
+	
+	if (cmdlist.size() < 2){
+		makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NEEDMOREPARAMS(client.getNickname(), "PART"));
+	}
+	else {
+		std::stringstream ss(cmdlist[1]);
+		std::string token;
+		while (getline(ss, token, ','))
+		{
+			token.erase(0, 1);
+			std::map<std::string, Channel>::iterator found = server.getChannels().find(token);
+			if (found == Server::getInstance().getChannels().end()){
+				makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NOSUCHCHANNEL(client.getNickname(), token));
+				continue;
+			}
+			targetChannel = &(found->second);
+			if (targetChannel->findJoinClient(client.getNickname()) == false){
+				makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NOTONCHANNEL(client.getNickname(), token));
+			}
+			for (std::vector<Client *>::iterator it = targetChannel->getClients().begin(); it != targetChannel->getClients().begin(); ++it){
+				makeWriteEvent((*it)->getUserSock(), server.getChangeList(), SUCCESS_REPL((*it)->getUserName(), (*it)->getHostName(), "127.0.0.1", mergeVec(cmdlist)));
+			}
+		} 
+	}
+	return;
+}
+bool  Part::checkArgs(std::vector<std::string>& cmdlist, Client& client){
 
 }
