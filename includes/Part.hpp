8967a4381f84153bc7e0f05a6415b4123e8cd66b
wsehyeon <wsehyeon21@gmail.com>
2023-02-23 21:14:09 +0900

    quit, part

--- includes/Part.hpp
+++ includes/Part.hpp
@@ -9,7 +9,6 @@ class Part : public Command
 		Part();
 		void  execute(std::vector<std::string>& cmdlist, Client& client, std::vector<struct kevent>& changelist, std::map<std::string, Channel>* channels);
 	    bool  checkArgs(std::vector<std::string>& cmdlist, Client& client);
-		
 };
 
 #endif
--- srcs/Part.cpp
+++ srcs/Part.cpp
@@ -3,6 +3,17 @@
 #include "../includes/Server.hpp"
 #include <sstream>
 
+void print_joinedClient(Channel * c);
+
+static void print_all_about_channel(Channel &c){
+	std::cout << "part call all about\n		channel address : " << &c << std::endl;
+    std::vector<Client *> clients = c.getClients();
+    std::cout << "		print all about " <<  c.getChannelName() << std::endl;
+    for (int i = 0; i != clients.size(); ++i){
+        std::cout << "		" << clients[i]->getNickname() << std::endl;
+    }
+}
+
 Part::Part() : Command(0){};
 
 void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
@@ -19,18 +30,29 @@ void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
 		while (getline(ss, token, ','))
 		{
 			token.erase(0, 1);
+
 			std::map<std::string, Channel>::iterator found = server.getChannels().find(token);
 			if (found == Server::getInstance().getChannels().end()){
 				makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NOSUCHCHANNEL(client.getNickname(), token));
 				continue;
 			}
 			targetChannel = &(found->second);
+			// print_all_about_channel(*targetChannel);
 			if (targetChannel->findJoinClient(client.getNickname()) == false){
 				makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NOTONCHANNEL(client.getNickname(), token));
 				continue;
 			}
 			for (std::vector<Client *>::iterator it = targetChannel->getClients().begin(); it != targetChannel->getClients().end(); ++it){
 				makeWriteEvent((*it)->getUserSock(), server.getChangeList(), SUCCESS_REPL("(*it)->getUserName()", "(*it)->getHostName()", "127.0.0.1", mergeVec(cmdlist)));
+				if ((*it)->getNickname() == client.getNickname()) // it's me
+				{
+					if (targetChannel->getClients().size() == 1)
+						server.getChannels().erase(token);
+					else{
+						targetChannel->getClients().erase(it);
+						it--;
+					}
+				}
 			}
 		} 
 	}
@@ -38,4 +60,13 @@ void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
 }
 bool  Part::checkArgs(std::vector<std::string>& cmdlist, Client& client){
 	return true;
+}
+
+
+
+void print_joinedClient(Channel * c){
+	std::cout << c << "[" <<  c->getChannelName() << "] ..... " << std::endl;
+	for (std::vector<Client *>::iterator it = c->getClients().begin(); it != c->getClients().end(); ++it){
+		std::cout << (*it)->getNickname() << std::endl;
+	}
 }
