361025cf8562c1d20fe2ad1709df9cd40abdcdfc
wsehyeon <wsehyeon21@gmail.com>
2023-02-24 16:05:44 +0900

    Modi : 채널삭제와 메세지 전송 분리

--- srcs/Quit.cpp
+++ srcs/Quit.cpp
@@ -16,15 +16,18 @@ void  Quit::execute(std::vector<std::string>& cmdlist, Client& client, std::vect
 			}
 		} 
 	}
+	// 내가 접속한 채널 돌면서 quit 메세지 전송
+	for (std::vector<Channel *>::iterator joinedChannelIt = client.getJoinedChannel().begin(); joinedChannelIt != client.getJoinedChannel().end(); ++joinedChannelIt ){
+		for (std::vector<Client *>::iterator in = (*joinedChannelIt)->getClients().begin(); in != (*joinedChannelIt)->getClients().end(); ++in)
+			makeWriteEvent((*in)->getUserSock(), server.getChangeList(), SUCCESS_REPL((*in)->getUserName(), (*in)->getHostName(), "127.0.0.1", mergeVec(cmdlist)));
+	}
+	
+	// 내가 제거된 채널에 더이상 클라이언트가 없으면 서버에서 제거
 	for (std::vector<Channel *>::iterator joinedChannelIt = client.getJoinedChannel().begin(); joinedChannelIt != client.getJoinedChannel().end(); ++joinedChannelIt ){
 		if ((*joinedChannelIt)->getClients().size() == 0)
 			server.getChannels().erase((*joinedChannelIt)->getChannelName());
-		else{
-			for (std::vector<Client *>::iterator in = (*joinedChannelIt)->getClients().begin(); in != (*joinedChannelIt)->getClients().end(); ++in){
-				makeWriteEvent((*in)->getUserSock(), server.getChangeList(), SUCCESS_REPL((*in)->getUserName(), (*in)->getHostName(), "127.0.0.1", mergeVec(cmdlist)));
-			} 
-		}
 	}
+	
 }
 
 bool  Quit::checkArgs(std::vector<std::string>& cmdlist, Client& client){
