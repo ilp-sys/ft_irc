041af3558ef1beb71933842da3058cc85e95c476
wsehyeon <wsehyeon21@gmail.com>
2023-02-27 14:20:13 +0900

    Fix : part 메세지를 나를 포함한 채널 구성원에게 보내기

--- srcs/Part.cpp
+++ srcs/Part.cpp
@@ -18,7 +18,6 @@ void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
 		while (getline(ss, token, ','))
 		{
 			token.erase(0, 1);
-			
 			std::map<std::string, Channel>::iterator found = channels->find(token);
 			if (found == Server::getInstance().getChannels().end()){
 				makeWriteEvent(client.getUserSock(), changelist, ERR_NOSUCHCHANNEL(client.getNickname(), token));
@@ -30,7 +29,13 @@ void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
 				makeWriteEvent(client.getUserSock(), server.getChangeList(), ERR_NOTONCHANNEL(client.getNickname(), token));
 				continue;
 			}
-
+			
+			if (targetChannel.getClients().size() > 0){
+				std::vector<std::string> reply = cmdlist;
+				reply[1] = "#" + targetChannel.getChannelName();
+				for (std::vector<Client *>::iterator it = targetChannel.getClients().begin(); it != targetChannel.getClients().end(); ++it)
+					makeWriteEvent((*it)->getUserSock(), server.getChangeList(), SUCCESS_REPL(client.getNickname(), mergeVec(reply)));
+			}
 			// 채널의 접속자 리스트에서 나를 찾아서 삭제
 			for (std::vector<Client *>::iterator it = targetChannel.getClients().begin(); it != targetChannel.getClients().end(); ++it){
 				if ((*it)->getNickname() == client.getNickname()){
@@ -40,13 +45,11 @@ void  Part::execute(std::vector<std::string>& cmdlist, Client& client, \
 					break;
 				}
 			}
-			// 타겟 채널에 아무도 없으면 채널삭제 || 채널이벤트 메세지를 채널 안에 보내기
+			
+			// 타겟 채널에 아무도 없으면 채널삭제
 			if (targetChannel.getClients().size() == 0)
 				server.getChannels().erase(targetChannel.getChannelName());
-			else{
-				for (std::vector<Client *>::iterator it = targetChannel.getClients().begin(); it != targetChannel.getClients().end(); ++it)
-					makeWriteEvent((*it)->getUserSock(), server.getChangeList(), SUCCESS_REPL(client.getNickname(), mergeVec(cmdlist)));
-			}
+
 			// client가 접속해있는 채널 리스트에서 타겟삭제
 			for (std::vector<Channel *>::iterator it = client.getJoinedChannel().begin(); it != client.getJoinedChannel().end(); ++it){
 				if ((*it)->getChannelName() == targetChannel.getChannelName()){
